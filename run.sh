d11f0107422c964caea0e3101d4875876f814d60
fatemehalipour <falipour@uwaterloo.ca>
2024-06-11 17:40:58 -0400

    run.sh updated

--- run.sh
+++ run.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #SBATCH --account=def-khill22
 #SBATCH --cpus-per-task=1
-#SBATCH --gpus-per-node=v100:1
-#SBATCH --mem=128000M
-#SBATCH --time=02:00:00
+#SBATCH --gpus-per-node=1
+#SBATCH --mem=64000M
+#SBATCH --time=1:00:00
 
 module avail python
 module load python/3.10
-virtualenv --no-download ENV
+#virtualenv --no-download ENV
 source ENV/bin/activate
 
-pip install -r requirements.txt
-python3 src/cluster.py --dataset="Cypriniformes.fasta"
+#pip install -r requirements.txt
+python3 src/cluster.py --dataset="01_Cypriniformes.fasta"
 
